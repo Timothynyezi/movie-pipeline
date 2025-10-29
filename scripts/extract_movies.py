992d948772e92914c528ed2a1a263333c45871b1
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-10-29 17:25:51 +0200

    wrote code for the if statement

--- scripts/extract_movies.py
+++ scripts/extract_movies.py
@@ -7,4 +7,8 @@ RAW_DIR =  Path(__file__).resolve().parents[1] / "data" / "raw"
 def main(src=None):
     RAW_DIR.mkdir(parents=True, exist_ok=True)
     if src:
+        dest = RAW_DIR / "movies.csv"
+        shutil.copy(src, dest)
+        print(f"Copied {src} -> {dest}")
+    else:
         pass
