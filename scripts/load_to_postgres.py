af1e35a892ce64ffe6a10b87ae22dc2d59feb33b
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-10-30 13:28:24 +0200

    created a proc variables and accessed it's path using the base , also created function

--- scripts/load_to_postgres.py
+++ scripts/load_to_postgres.py
@@ -3,3 +3,8 @@ from pathlib import Path
 
 BASE = Path(__file__).resolve().parent[1]
 RAW = BASE / "data" / "raw" / "movies.csv"
+PROC = BASE / "data" / "processed" / "movies_processed.csv"
+PROC.parent.mkdir(parents=True, exist_ok=True)
+
+def categorize_rating(r):
+    pass
