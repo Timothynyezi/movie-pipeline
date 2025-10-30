f0f316308f7100b40722d68147ae83aabf66adc8
Thamsanqa <thaminyezi@gmail.com>
2025-10-30 17:10:23 +0200

    wrote code to make sure that the processed folder exists

--- scripts/extract_movies.py
+++ scripts/extract_movies.py
@@ -4,3 +4,6 @@ from pathlib import Path
 RAW = Path("data/raw/movies.csv")
 PROCESSED_DIR = Path("data/processed")
 PROCESSED_PATH = PROCESSED_DIR / "movies_processed.csv"
+
+PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
+
