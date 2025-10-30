532e28e09859ccb09f8d04f4ceb20d3fc2a7aaec
Thamsanqa <thaminyezi@gmail.com>
2025-10-30 17:12:29 +0200

    initial logic to read raw csv file

--- scripts/extract_movies.py
+++ scripts/extract_movies.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from pathlib import Path
 
-RAW = Path("data/raw/movies.csv")
+RAW_PATH= Path("data/raw/movies.csv")
 PROCESSED_DIR = Path("data/processed")
 PROCESSED_PATH = PROCESSED_DIR / "movies_processed.csv"
 
 PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
 
+if RAW_PATH.exists():
+    print(f"Found raw file: {RAW_PATH}")
